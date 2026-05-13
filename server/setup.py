7257e9d41d9862753b5d3b2bca54daeb38f6c5ef
abhi1202803 <abhishek87779@gmail.com>
2026-05-13 12:41:26 +0530

    update setup.py

--- server/setup.py
+++ server/setup.py
@@ -1,8 +1,8 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name="careai",
-    version="0.1",
+    name="medirag",
+    version="0.0.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
